809ae31d04e635056c719e47d0538936ae06615a
phan2796 <phan2796@gmail.com>
2020-03-12 15:36:59 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         PHAN PHAN PHAN
         </p>
         <a
           className="App-link"
